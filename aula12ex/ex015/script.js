8b753627e6f2b4e751b836150bd33467d417a3e0
Larissa Romualdo <larissamaria.romualdo@yahoo.com.br>
2025-11-27 18:50:21 -0300

    exercicios javascript

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,1 +1,49 @@
-alert('Olá!')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente.')
+    } else {
+       var fsex = document.getElementsByName('radsex')
+       var idade = ano - Number(fano.value)
+       var gênero = ''
+       var img = document.createElement('img')
+       img.setAttribute('id', 'foto')
+       if(fsex[0].checked) {
+        gênero = 'Homem'
+        if(idade >= 0 && idade < 10) {
+            //criança
+            img.setAttribute('src','crianca-homem.editado.jpg')
+        } else if (idade < 21) {
+            //adulto
+            img.setAttribute('src','adulto-homem.editado.jpg')
+        }else if (idade < 50) {
+            //adulto
+            img.setAttribute('src','adulto-homem.editado.jpg')
+        } else {
+            //idoso
+            img.setAttribute('src','idoso-homem.editado.jpg')
+        }
+       } else if (fsex[1].checked) {
+        gênero = 'Mulher'
+        if(idade >= 0 && idade < 10) {
+            //criança
+            img.setAttribute('src', 'crianca-mulher.editado.jpg')
+        } else if (idade < 21) {
+            //adulto
+            img.setAttribute('src', 'adulto-mulher.editado.jpg')
+        }else if (idade < 50) {
+            //adulto
+            img.setAttribute('src', 'adulto-mulher.editado.jpg')
+        } else {
+            //idoso
+            img.setAttribute('src', 'idoso-mulher.editado.jpg')
+        }
+       }
+       res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appenChild(img)
+    }
+}
