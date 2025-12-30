e3793d6fdf58bd2d15d77a13d81dc940b5ae78da
Larissa Romualdo <larissamaria.romualdo@yahoo.com.br>
2025-12-30 17:02:18 -0300

    aula 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+/* 
+console.log('Tudo bem?')
+
+*/
+
+/* modelo de estrutura de repetição com teste logico no inicio:
+var c = 1 
+while (c <= 3) {
+    console.log(`Passo ${c}`)
+    c++ 
+}
+*/ 
+
+var c = 1 
+do {
+    console.log(`Passo ${c}`)
+    c++ 
+} while (c <= 6)
---
+++ aula14/ambiente.js
@@ -0,0 +1,30 @@
+/*
+var c = 1
+while (c <= 5 ) {
+    console.log(c)
+    c++
+} 
+*/
+
+
+/*
+var c = 1
+do {
+    console.log(c)
+    c++
+} while (c <= 5)
+*/
+
+/*
+for(var c = 1; c <= 5; c++) {
+    console.log(c)
+}
+
+dos tres jeitos que escrevi o resultado é o mesmo.
+*/
+
+console.log('Vai começar...')
+for(var c = 1;c <= 4;c++) {
+    console.log(c)
+}
+console.log('FIM')
