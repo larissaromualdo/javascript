cd6c19f36590c475f69314853b24686341437912
Larissa Romualdo <larissamaria.romualdo@yahoo.com.br>
2026-02-06 14:19:24 -0300

    ex15

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,4 +1,10 @@
 let num = [5, 8, 2, 9, 3]
-
+num.sort()
 console.log(`nosso vetor tem ${num.length} posicoes`)
 console.log(num[0])
+
+let pos = num.indexOf(4)
+if (pos == -1) {
+    console.log(`esse número não existe`)
+}
+console.log(`O valor 4 está na posição ${pos}`)
