7828eee53c81460e7c091e4469b5000ed84fecb7
Larissa Romualdo <larissamaria.romualdo@yahoo.com.br>
2026-02-12 11:36:51 -0300

    ex 16

---
+++ aula16ex/ex16/script.js
@@ -0,0 +1,8 @@
+function analisador() {
+    let num = document.getElementById('num')
+    let ret = document.getElementById('retorno')
+
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número')
+    }
+}
